2504b1a0e3f90cf2a589d31d0f37e451dfe4f606
derkrisz <krisztian.dergmail.com>
2017-11-23 11:55:00 +0000

    initial commit/populates students

---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+require_relative('../models/student.rb')
+
+
+student1 = Student.new({
+  'first_name' => 'George',
+  'last_name' => 'Foreman',
+  'house' => 'Fire',
+  'age' => 30
+  })
+
+student2 = Student.new({
+  'first_name' => 'Donald',
+  'last_name' => 'Trump',
+  'house' => 'Air',
+  'age' => 70
+  })
+
+student3 = Student.new({
+  'first_name' => 'Kim',
+  'last_name' => 'Jong Un',
+  'house' => 'Water',
+  'age' => 25
+  })
+
+student4 = Student.new({
+  'first_name' => 'Viktor',
+  'last_name' => 'Orban',
+  'house' => 'Earth',
+  'age' => 50
+  })
+
+student1.save
+student2.save
+student3.save
+student4.save
